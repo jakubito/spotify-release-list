9afc3444ac26319a550168562ff838cf32daaaee
Jakub Dobes <dobes.jakub@gmail.com>
2020-06-16 15:54:35 -0700

    Register PWA service worker

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { Router, Redirect } from '@reach/router';
 import * as Sentry from '@sentry/browser';
 import '@fortawesome/fontawesome-free/js/all';
 import 'react-dates/initialize';
-import Auth from 'components/Auth';
-import App from 'components/App';
+import * as serviceWorker from 'serviceWorker';
 import { store, hydrate } from 'store';
 import { getSettingsTheme } from 'selectors';
+import Auth from 'components/Auth';
+import App from 'components/App';
 import 'styles/index.scss';
 
 Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN });
+
+serviceWorker.register();
 
 function applyTheme() {
   const theme = getSettingsTheme(store.getState());
