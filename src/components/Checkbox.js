42166a07ce8d5e92dc933481078550a409118fdb
Jakub Dobes <dobes.jakub@gmail.com>
2020-12-09 09:21:02 -0800

    Add checkbox component

---
+++ src/components/Checkbox.js
@@ -0,0 +1,49 @@
+import classNames from 'classnames'
+
+/**
+ * Render checkbox input
+ *
+ * @param {{
+ *   id?: string
+ *   name?: string
+ *   className?: string
+ *   label?: string
+ *   value?: string
+ *   checked?: boolean
+ *   defaultChecked?: boolean
+ *   onChange?: React.ChangeEventHandler<HTMLInputElement>
+ *   dark?: boolean
+ * }} props
+ */
+function Checkbox({ id, name, className, label, value, checked, defaultChecked, onChange, dark }) {
+  return (
+    <>
+      <input
+        type="checkbox"
+        id={id}
+        name={name}
+        value={value}
+        checked={checked}
+        defaultChecked={defaultChecked}
+        onChange={onChange}
+        className={classNames(className, 'is-checkradio', 'is-circle', 'has-background-color', {
+          'is-white': !dark,
+          'is-dark': dark,
+        })}
+      />
+      {label && (
+        <label
+          htmlFor={id}
+          className={classNames('has-text-weight-semibold', {
+            'has-text-light': !dark,
+            'has-text-grey': dark,
+          })}
+        >
+          {label}
+        </label>
+      )}
+    </>
+  )
+}
+
+export default Checkbox
