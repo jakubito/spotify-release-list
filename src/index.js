74a792eacb64a31fdb2db85e11a3c1b420b3272f
Jakub Dobes <dobes.jakub@gmail.com>
2022-05-11 12:40:02 -0700

    Update packages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import PWAPrompt from 'react-ios-pwa-prompt'
@@ -7,7 +7,7 @@ import moment from 'moment'
 import 'react-dates/initialize'
 import * as serviceWorkerRegistration from 'serviceWorkerRegistration'
 import { store, hydrate } from 'state'
-import { getSettings, getSettingsTheme } from 'state/selectors'
+import { getSettings } from 'state/selectors'
 import { updateReady } from 'state/actions'
 import { Routes } from 'components'
 import 'styles/index.scss'
@@ -15,30 +15,21 @@ import 'styles/index.scss'
 Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN })
 serviceWorkerRegistration.register({ onUpdate: () => store.dispatch(updateReady()) })
 
-hydrate.then(applyTheme).then(setFirstDayOfWeek).then(renderApp)
+const container = document.getElementById('root')
+const root = createRoot(container)
 
-function applyTheme() {
-  const theme = getSettingsTheme(store.getState())
+hydrate.then(() => {
+  const { theme, firstDayOfWeek } = getSettings(store.getState())
 
-  if (theme) {
-    document.documentElement.classList.add(...theme.split(' '))
-  }
-}
-
-function setFirstDayOfWeek() {
-  const { firstDayOfWeek } = getSettings(store.getState())
-
+  if (theme) document.documentElement.classList.add(...theme.split(' '))
   moment.updateLocale(moment.locale(), { week: { dow: firstDayOfWeek } })
-}
 
-function renderApp() {
-  render(
+  root.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes />
         <PWAPrompt />
       </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
   )
-}
+})
