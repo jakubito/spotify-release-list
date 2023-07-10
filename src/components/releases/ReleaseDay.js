7807efad8c981c18ba299edd117d589600cd7d9f
Jakub Dobes <dobes.jakub@gmail.com>
2023-07-09 17:15:37 -0700

    Display release day bullet if a new track is included

--- src/components/releases/ReleaseDay.js
+++ src/components/releases/ReleaseDay.js
@@ -2,6 +2,7 @@ import { memo } from 'react'
 import { useSelector } from 'react-redux'
 import moment from 'moment'
 import { getPreviousSyncMaxDate, getSettings } from 'state/selectors'
+import { albumsHistory, albumsNew } from 'albums'
 import Album from './Album'
 
 /**
@@ -12,12 +13,24 @@ import Album from './Album'
 function ReleaseDay({ date, albums }) {
   const { trackHistory } = useSelector(getSettings)
   const previousSyncMaxDate = useSelector(getPreviousSyncMaxDate)
-  const showBullet = !trackHistory && previousSyncMaxDate && date > previousSyncMaxDate
+
+  const showBullet = () => {
+    if (trackHistory) {
+      if (albumsNew.size === 0) return false
+      if (albumsHistory.size === 0) return false
+      for (const album of albums) {
+        if (albumsNew.has(album.id)) return true
+      }
+      return false
+    }
+    if (!previousSyncMaxDate) return false
+    return date > previousSyncMaxDate
+  }
 
   return (
     <div className="ReleaseDay columns is-gapless">
       <div className="ReleaseDay__date column">
-        {showBullet && (
+        {showBullet() && (
           <span className="ReleaseDay__bullet icon">
             <i className="fas fa-circle" />
           </span>
